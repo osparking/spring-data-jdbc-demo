68daf40eec5f571461f120a6f662399dcf4a2b42
osparking <jbpark03@gmail.com>
2023-02-20 15:31:22 +0900

    Chess game repository interface

---
+++ src/main/java/space/bumtiger/sd_jdbc_demo/repository/ChessGameRepository.java
@@ -0,0 +1,14 @@
+package space.bumtiger.sd_jdbc_demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import space.bumtiger.sd_jdbc_demo.domain.ChessGame;
+
+// @formatter:off
+public interface ChessGameRepository 
+				extends CrudRepository<ChessGame, Long> {
+	List<ChessGame> findByPlayBlack(String playerBlack);
+// @formatter:on
+}
