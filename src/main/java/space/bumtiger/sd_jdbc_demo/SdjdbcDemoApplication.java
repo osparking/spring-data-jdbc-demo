24ebcd96e3429e7701c76726bc2d09c4e5ec4691
osparking <jbpark03@gmail.com>
2023-02-21 21:54:51 +0900

    read games using black player name

--- src/main/java/space/bumtiger/sd_jdbc_demo/SdjdbcDemoApplication.java
+++ src/main/java/space/bumtiger/sd_jdbc_demo/SdjdbcDemoApplication.java
@@ -1,16 +1,20 @@
 package space.bumtiger.sd_jdbc_demo;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
 import space.bumtiger.sd_jdbc_demo.domain.ChessGame;
 import space.bumtiger.sd_jdbc_demo.domain.ChessMove;
 import space.bumtiger.sd_jdbc_demo.domain.ChessMove.MoveColor;
 import space.bumtiger.sd_jdbc_demo.repository.ChessGameRepository;
 
+@Slf4j
 @SpringBootApplication
 public class SdjdbcDemoApplication {
 
@@ -26,7 +30,7 @@ public class SdjdbcDemoApplication {
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 			ChessGame game = new ChessGame();
 			game.setPlayerWhite("Thorben Janssen");
-			game.setPlayerBlack("A strong player");
+			game.setPlayerBlack("A weak player");
 
 			ChessMove move1white = new ChessMove();
 			move1white.setColor(MoveColor.WHITE);
@@ -39,6 +43,9 @@ public class SdjdbcDemoApplication {
 			game.getMoves().add(move1Black);
 
 			gameRepo.save(game);
+			
+			List<ChessGame> games = gameRepo.findByPlayerBlack("A weak player");
+			games.forEach(g -> log.info(g.toString()));
 		};
 	}
 }
