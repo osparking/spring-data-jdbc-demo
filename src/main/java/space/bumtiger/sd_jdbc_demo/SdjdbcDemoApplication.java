1c0a991b2f83414e6561bd5fc831f61426b57648
osparking <jbpark03@gmail.com>
2023-02-21 11:05:22 +0900

    persisting aggregates

--- src/main/java/space/bumtiger/sd_jdbc_demo/SdjdbcDemoApplication.java
+++ src/main/java/space/bumtiger/sd_jdbc_demo/SdjdbcDemoApplication.java
@@ -1,7 +1,15 @@
 package space.bumtiger.sd_jdbc_demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import space.bumtiger.sd_jdbc_demo.domain.ChessGame;
+import space.bumtiger.sd_jdbc_demo.domain.ChessMove;
+import space.bumtiger.sd_jdbc_demo.domain.ChessMove.MoveColor;
+import space.bumtiger.sd_jdbc_demo.repository.ChessGameRepository;
 
 @SpringBootApplication
 public class SdjdbcDemoApplication {
@@ -10,4 +18,29 @@ public class SdjdbcDemoApplication {
 		SpringApplication.run(SdjdbcDemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx,
+			ChessGameRepository gameRepo) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			ChessGame game = new ChessGame();
+			game.setPlayerWhite("Thorben Janssen");
+			game.setPlayerBlack("A strong player");
+
+			ChessMove move1white = new ChessMove();
+			move1white.setMoveNumber(1);
+			move1white.setColor(MoveColor.WHITE);
+			move1white.setMove("e4");
+			game.getMoves().add(move1white);
+
+			ChessMove move1Black = new ChessMove();
+			move1Black.setMoveNumber(1);
+			move1Black.setColor(MoveColor.BLACK);
+			move1Black.setMove("e5");
+			game.getMoves().add(move1Black);
+
+			gameRepo.save(game);
+		};
+	}
 }
